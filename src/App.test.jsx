562466595a45d0b05372a3d764596ae396cfefac
nikolaskakavac <nikolaskakavac22@gmail.com>
2026-02-23 22:56:59 +0100

    Test sada koristi MemoryRouter, rešena greška sa useLocation u testiranju.

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,10 +1,16 @@
 
+import React from 'react';
 import { render, screen } from '@testing-library/react';
 import { test, expect } from 'vitest';
+import { MemoryRouter } from 'react-router-dom';
 import App from './App';
 
 test('renders Jobzee app main heading', () => {
-  render(<App />);
+  render(
+    <MemoryRouter>
+      <App />
+    </MemoryRouter>
+  );
   const heading = screen.getByText(/jobzee/i);
   expect(heading).toBeInTheDocument();
 });
